f700d52ad9b010084fcba106c5b483b5348c35e4
y0ADQ1 <juanderasduron@gmail.com>
2024-11-12 12:48:36 -0600

    implementacion de la migracion stock, junto con su modelo

---
+++ app/Models/Stock.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Stock extends Model
+{
+    use HasFactory;
+
+    protected $table = 'stock';
+
+    protected $fillable = [
+        'stock',
+        'id_menu',
+    ];
+
+    /**
+     * 
+     */
+    public function menu()
+    {
+        return $this->belongsTo(Menu::class, 'id_menu', 'id_menu');
+    }
+}
---
+++ database/migrations/2024_11_12_183808_create_stock_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('stock', function (Blueprint $table) {
+            $table->id('id_stock');
+            $table->integer('stock');
+            $table->foreignId('id_menu')->nullable()->constrained('menu', 'id_category');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('stock');
+    }
+};
