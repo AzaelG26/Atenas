9ee1e57d30156c4f994b0624a3b2854fce249bf4
y0ADQ1 <juanderasduron@gmail.com>
2024-11-21 11:54:55 -0600

    modificacion en las migraciones

--- database/migrations/2024_11_07_234114_create_order_details_table.php
+++ database/migrations/2024_11_07_234114_create_order_details_table.php
@@ -15,9 +15,8 @@ return new class extends Migration
             $table->integer('quantity')->nullable(false);
             $table->text('notes')->nullable();
             $table->text('specifications')->nullable();
-            $table->enum('status', ['Pending', 'In Process', 'Completed', 'Canceled', 'Delivered'])->nullable(false);
-            $table->timestamp('created_at')->nullable(false)->useCurrent(); // Fecha de pedido
-            $table->timestamp('delivered_at')->nullable(); // Fecha de entrega
+            $table->timestamp('created_at')->nullable(false)->useCurrent(); 
+            $table->timestamp('delivered_at')->nullable(); 
 
             $table->foreign('id_order')->references('id_order')->on('orders');
             $table->foreign('id_menu')->references('id_menu')->on('menu');
