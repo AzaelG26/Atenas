a69e2cfdbfdad96ee927ee1abb1a573aeb9857a9
y0ADQ1 <juanderasduron@gmail.com>
2024-12-02 13:57:33 -0600

    modificacion de la migracion de reseñas, y se elimino la otra migracion xd

--- database/migrations/2024_11_20_164510_create_reseñas_table.php
+++ database/migrations/2024_11_20_164510_create_reseñas_table.php
@@ -16,7 +16,8 @@ class CreateReseñasTable extends Migration
         Schema::create('reviews', function (Blueprint $table) {
             $table->id();
             $table->string('review'); // Mantener uno de los campos `review`.
-            $table->foreignId('usuario_id')->constrained('users')->onDelete('cascade');
+            $table->unsignedBigInteger('usuario_id')->nullable(); // Ahora es nullable
+            $table->foreign('usuario_id')->references('id')->on('users')->onDelete('cascade');
             $table->tinyInteger('satisfaction_level') // Nuevo campo para nivel de satisfacción.
                 ->unsigned()
                 ->checkBetween(1, 5); // Agregar restricción para valores entre 1 y 5.
